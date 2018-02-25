5e4cd6cc4e3d45669596f56b88824b06760d8f37
HoangLM <minhhoangtoday@gmail.com>
2018-02-25 14:19:22 +0700

    mod. change connection sample

--- sample/src/main/java/com/hoanglm/rxandroidjmdns/DeviceActivity.java
+++ sample/src/main/java/com/hoanglm/rxandroidjmdns/DeviceActivity.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.hoanglm.rxandroidjmdns.socket_device.RxSocketDevice;
 import com.hoanglm.rxandroidjmdns.socket_device.connection.RxSocketConnection;
 import com.hoanglm.rxandroidjmdns.utils.ConnectionSharingAdapter;
 import com.hoanglm.rxandroidjmdns.utils.RxJmDNSLog;
+import com.hoanglm.rxandroidjmdns.utils.StringUtil;
+import com.trello.rxlifecycle.android.ActivityEvent;
 import com.trello.rxlifecycle.components.RxActivity;
 
 import java.util.Locale;
@@ -18,6 +21,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.subjects.PublishSubject;
 
 public class DeviceActivity extends RxActivity {
@@ -39,6 +43,12 @@ public class DeviceActivity extends RxActivity {
     @BindView(R.id.connect_btn)
     Button mConnectButton;
 
+    @BindView(R.id.status_connection_txt)
+    TextView mStatusConnectionTextView;
+
+    @BindView(R.id.send_edt)
+    EditText mSendEditText;
+
     private String ipAddress;
     private int port;
     private RxSocketDevice mRxSocketDevice;
@@ -56,7 +66,13 @@ public class DeviceActivity extends RxActivity {
         mRxSocketDevice = ((MainApplication) getApplication()).getRxSocketService().getSocketDevice(ipAddress, port);
         mRxSocketConnectionObservable = prepareConnectionObservable();
         mAddressTextView.setText(String.format(Locale.US, "host = %s-%d", ipAddress, port));
-        mRxSocketDevice.observeConnectionStateChanges().subscribe(rxSocketConnectionState -> RxJmDNSLog.d("Connection state -> " +rxSocketConnectionState));
+        mRxSocketDevice.observeConnectionStateChanges()
+                .observeOn(AndroidSchedulers.mainThread())
+                .compose(bindUntilEvent(ActivityEvent.DESTROY))
+                .subscribe(rxSocketConnectionState -> {
+            RxJmDNSLog.d("Connection state -> " + rxSocketConnectionState);
+            mStatusConnectionTextView.setText(rxSocketConnectionState.toString());
+        });
 
     }
 
@@ -74,12 +90,25 @@ public class DeviceActivity extends RxActivity {
         } else {
             mRxSocketConnectionObservable
                     .doOnSubscribe(() -> RxJmDNSLog.d("CONNECTING"))
+                    .compose(bindUntilEvent(ActivityEvent.DESTROY))
                     .subscribe(rxSocketConnection ->
                                     RxJmDNSLog.d("Connect success"),
                             throwable -> RxJmDNSLog.e(throwable, "connect failed"),
                             () -> RxJmDNSLog.d("Connect completed")
                     );
         }
+    }
+
+    @OnClick(R.id.send_btn)
+    void sendClicked() {
+        String text = mSendEditText.getText().toString();
+        mRxSocketConnectionObservable
+                .doOnSubscribe(() -> RxJmDNSLog.d("send " + text))
+                .flatMap(rxSocketConnection -> rxSocketConnection.sendMessage(StringUtil.convertStringToByte(text)))
+                .compose(bindUntilEvent(ActivityEvent.DESTROY))
+                .subscribe(data -> {
+                    RxJmDNSLog.d("send success " + StringUtil.convertByteToString(data));
+                }, throwable -> RxJmDNSLog.e(throwable, "send failed"));
     }
 
     private boolean isConnected() {
